6ddc6e81ef5b5f05052d0286e0a9ed504be0585d
dlanik <98417859+dlanik@users.noreply.github.com>
2022-03-16 19:56:43 +0100

    Completed exercise 1 and 2 of Intro to jQuery

---
+++ Intro-to-jQuery/Exercise-1/script.js
@@ -0,0 +1,4 @@
+$("#Velox").text("Macrotis");
+$("#Zerda").css("border-color", "purple");
+// $("#Velox, #Zerda, #Lagopus").css("background-color", "yellow");
+$("p").css("background-color", "yellow");
