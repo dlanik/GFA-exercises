c13793736734afb7df0e3499e0eee6180a4a0296
dlanik <98417859+dlanik@users.noreply.github.com>
2022-03-19 19:24:13 +0100

    Complete functionality and some css changes

--- Todo list/script.js
+++ Todo list/script.js
@@ -1,18 +1,24 @@
-// probably not the best way to get about this
+// probably not the best way to go about this
 
 // using "n" to assign unique ID to each item
 let n = 0;
 
 // clicking button creates item from input field with n ID, empties the input field and increments n
-$("button").click(() => {
-    $("ul").append(`<li class="item" id=${n}>${$("input").val()}</li>`);
-    $("input").val("");
+$("#add-btn").click(() => {
+    if ($("input").val() !== ""){
+        $("ul").append(`<li class="item" id=${n}>${$("input").val()}<button type="button" class="dlt-btn" id="dlt-btn${n}"></button></li>`);
+        $("input").val("");
     // console.log(`#${n}`);
-    n++;
+        n++;
+    }
 });
 
-// honestly this almost drove me crazy, it's working but i don't think i fully understand
+// honestly this almost drove me crazy, it's working but i don't think i understand
 // clicking item toggles "completed" class (line-through)
 $('#todo-list').on('click', '.item', function(event) {
-    $(`#${event.target.id}`).toggleClass("completed");
+    $(event.target).toggleClass("completed");
+});
+
+$('#todo-list').on('click', '.dlt-btn', function(event) {
+    ((event.target).parentElement).remove();
 });
